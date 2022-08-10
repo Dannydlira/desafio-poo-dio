abc82c6fbbcb49e0af4aaed30e6c237d865b0c3f
Dannydlira <dannydlira@gmail.com>
2022-08-09 23:39:51 -0300

    comite final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+import com.sun.source.tree.ConditionalExpressionTree;
 
 import java.time.LocalDate;
 
@@ -16,13 +16,47 @@ public class Main {
         curso2.setDescricao("Descrição do cursoc de JavaScript");
         curso2.setCargaHoraria(120);
 
-         Mentoria mentoria = new Mentoria();
-            mentoria.setTitulo("Mentoria de Java");
-            mentoria.setDescricao("Descrição mentoria de Java");
-            mentoria.setData(LocalDate.now());
+         Conteudo conteudo1 = new Curso();
 
-         System.out.println(curso1);
-         System.out.println(curso2);
-         System.out.println(mentoria);
+         Mentoria mentoria = new Mentoria();
+         mentoria.setTitulo("Mentoria de Java");
+         mentoria.setDescricao("Descrição mentoria de Java");
+         mentoria.setData(LocalDate.now());
+
+//         System.out.println(curso1);
+//         System.out.println(curso2);
+//         System.out.println(mentoria);
+
+         Bootcamp bootcamp = new Bootcamp();
+         bootcamp.setNome("Bootcamp Java Developer");
+            bootcamp.setDescricao("Descrição do Bootcamp de Java");
+            bootcamp.getConteudos().add(curso1);
+            bootcamp.getConteudos().add(curso2);
+            bootcamp.getConteudos().add(mentoria);
+
+         Dev devDaniela = new Dev();
+         devDaniela.setNome("Daniela");
+         devDaniela.inscreverBootcamp(bootcamp);
+         System.out.println("Conteúdos Inscritos Daniela" + devDaniela.getConteudosInscritos());
+         devDaniela.progredir();
+         devDaniela.progredir();
+         devDaniela.progredir();
+         System.out.println("-");
+         System.out.println("Conteúdos Inscritos Daniela" + devDaniela.getConteudosConcluidos());
+         System.out.println("Conteúdos Concluídos Daniela" + devDaniela.getConteudosConcluidos());
+         System.out.println("Xp:"  + devDaniela.calcularTotalXp());
+
+         System.out.println("--------");
+
+         Dev devHeloisa = new Dev();
+         devHeloisa.setNome("Heloisa");
+         devHeloisa.inscreverBootcamp(bootcamp);
+         System.out.println("Conteúdos Inscritos Heloisa" + devHeloisa.getConteudosInscritos());
+         devHeloisa.progredir();
+         devHeloisa.progredir();
+         System.out.println("-");
+         System.out.println("Conteúdos Inscritos Heloisa" + devHeloisa.getConteudosInscritos());
+         System.out.println("Conteúdos Concluídos Heloisa" + devHeloisa.getConteudosConcluidos());
+         System.out.println("Xp:"  + devHeloisa.calcularTotalXp());
     }
 }
